1d1828961209d5b142dff4cda380c8bee8e54168
st58594 <st58594@student.upce.cz>
2021-01-21 11:32:28 +0100

    Pripojeni

---
+++ SemPrace/classes/Pripojeni.php
@@ -0,0 +1,20 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_NAME", "db_semiwww");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+
+class Connection
+{
+    static private $instance = NULL;
+
+    static function getPdoInstance() : PDO
+    {
+        if (self::$instance == NULL){
+            $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME."", DB_USER, DB_PASSWORD);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$instance = $conn;
+        }
+        return self::$instance;
+    }
+}
