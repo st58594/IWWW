cd07265c7fbac38d8a48d8af83f9e6054b7502ed
st58594 <st58594@student.upce.cz>
2021-01-27 00:09:09 +0100

    emulator

---
+++ SemPrace/controller/c_emulator.php
@@ -0,0 +1,20 @@
+<?php
+try {
+    if (isset($_POST["emulator"])){
+        if (empty($_SESSION["emulator"]))
+            $_SESSION["emulator"] = $_SESSION["Umelec"];
+        $_SESSION["Umelec"] = Umelec::getUmelec($_POST["id_umelec"]);
+        header("Location: ./index.php");
+        exit();
+    } elseif (isset($_POST["opravneni"])) {
+        Umelec::updateOpravneni($_POST["id_umelec"], $_POST["opravneni"]);
+    } elseif (!empty($_SESSION["emulator"])){
+        $_SESSION["Umelec"] = $_SESSION["emulator"];
+        unset($_SESSION["emulator"]);
+    }
+    header("Location: ./sprava.php?page=emulator");
+}catch (PDOException $exception){
+    header("Location: ./sprava.php?page=emulator&err=" . $exception->getMessage());
+}
+
+
---
+++ SemPrace/pages/sprava/emulator.php
@@ -0,0 +1,51 @@
+<?php
+?>
+<h3>Emulator</h3>
+<table class="emulator">
+    <thead>
+    <tr>
+        <th>login</th>
+        <th>Jmeno</th>
+        <th>Prijmeni</th>
+        <th>email</th>
+        <th>oprávnění</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach (Umelec::getAll() as $item) {
+        ?>
+        <tr>
+            <form class="table-view" method="post" action="./sprava.php">
+                <input type="hidden" name="id_umelec" value="<?php echo $item["id_umelec"]; ?>">
+                <td>
+                    <label><?php echo $item["login"];?></label>
+                </td>
+                <td>
+                    <label><?php echo $item["jmeno"];?></label>
+                </td>
+                <td>
+                    <label><?php echo $item["prijmeni"];?></label>
+                </td>
+                <td>
+                    <label><?php echo $item["email"];?></label>
+                </td>
+                <td>
+                    <select name="opravneni" onchange="this.form.submit()">
+                        <option value="ADMIN" <?php if ($item["opravneni"] == 'ADMIN') echo "selected";?>>Admin</option>
+                        <option value="UMELEC" <?php if ($item["opravneni"] == 'UMELEC') echo "selected";?>>Umělec</option>
+                    </select>
+                </td>
+                <td>
+                    <button type="submit" name="emulator" class="submit-row">
+                        <i class="fa fa-eye"></i>
+                    </button>
+                </td>
+            </form>
+        </tr>
+        <?php
+    }
+    ?>
+    </tbody>
+</table>
